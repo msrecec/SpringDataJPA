dfdaf6ef034bfc7b78586565d2c4d00fc0912a91
msrecec <67606889+msrecec@users.noreply.github.com>
2021-09-05 20:53:38 +0200

    added many to one relationship

--- src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
+++ src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/CourseRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.dailycodebuffer.spring.data.jpa.tutorial.repository;
 
 import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Course;
+import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Teacher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +22,22 @@ class CourseRepositoryTest {
         List<Course> courses = courseRepository.findAll();
 
         System.out.println("courses " + courses);
+    }
+
+    @Test
+    public void saveCourseWithTeacher() {
+        Teacher teacher = Teacher.builder()
+                .firstName("Priyanka")
+                .lastName("Singh")
+                .build();
+
+        Course course = Course.builder()
+                .title("Python")
+                .credit(6)
+                .teacher(teacher)
+                .build();
+
+        courseRepository.save(course);
     }
 
 
