b951dba842c25ed993bdc86da8154dd0a61489ad
msrecec <67606889+msrecec@users.noreply.github.com>
2021-09-05 14:14:32 +0200

    added student repository

---
+++ src/test/java/com/dailycodebuffer/spring/data/jpa/tutorial/repository/StudentRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.dailycodebuffer.spring.data.jpa.tutorial.repository;
+
+import com.dailycodebuffer.spring.data.jpa.tutorial.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void saveStudent() {
+        Student student = Student.builder()
+                .emailId("shabbir@gmail.com")
+                .firstName("Shabbir")
+                .lastName("Dawoodi")
+                .guardianName("Nikhil")
+                .guardianEmail("nikhil@gmail.com")
+                .guardianMobile("9999999999")
+                .build();
+
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void printAllStudent() {
+        List<Student> studentList = studentRepository.findAll();
+
+        System.out.println("studentList = " + studentList);
+    }
+
+}
